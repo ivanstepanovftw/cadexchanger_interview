3652e9b6c22cb31e017d0d5ba3ce9236937da8d2
Ivan Stepanov <ivanstepanovftw@gmail.com>
2019-05-29 15:09:55 +0300

    v1.0: inheritance showcase, helix implementation

--- application.cc
+++ application.cc
@@ -1,43 +1,10 @@
-/*
- * (Prepended '+' means 'done')
- *
- * Design a small program in C++ that would implement the following:
- * 1. Support a few types of 3D geometric curves – circles, ellipses and 3D helixes. (Simplified
- *    definitions are below). Each curve should be able to return a 3D point and a first derivative (3D
- *    vector) per parameter t along the curve.
- * 2. Populate a container (e.g. vector or list) of objects of these types created in random manner with
- *    random parameters. Ensure that the container will contain objects of all supported types.
- * 3. Print coordinates of points and derivatives of all curves in the container at t=PI/4.
- * 4. Populate a second container that would contain only circles from the first container. Make sure the
- *    second container shares (i.e. not clones) circles of the first one, e.g. via pointers.
- * 5. Sort the second container in the ascending order of circles’ radii. That is, the first element has the
- *    smallest radius, the last - the greatest.
- * 6. Compute the total sum of radii of all curves in the second container
- *
- * Additional optional requirements:
- * +7. Split implementation into a library of curves and executable which uses API of this library.
- *
- * Expectations to the implementation:
- * +1. The implementation must use virtual methods.
- * +2. Has neither explicit memory deallocation nor memory leaks.
- * +3. Curves must be physically correct (e.g. radii must be positive).
- * 4. Containers and sorting must be implemented using STL (C++ Standard Template Library).
- * +5. The implementation may use constructs of C++11 or higher.
- * +6. The code must compile with any compiler of your choice (gcc, Visual C++, etc).
- * +7. The result should be sent via email or posted on github. The delivery must contain source code only
- *    (no compiled binaries): *.h and *.cpp files and project files, so that we can compile the result by
- *    ourselves
- *
- * Curve definitions:
- * - All curves are parametrically defined, i.e. a point is calculated using some C(t) formula.
- * - Circle is planar in the plane XoY (i.e. all Z-coordinates are 0) and is defined by its radius.
- * - Ellipse is planar in the plane XoY and is defined by its two radii, along X and Y axes.
- * - Helix is spatial and is defined by its radius and step (see the figure below). It takes 2 * PI in
- *   parametric space to make a round, i.e. any point on helix satisfies the condition C(t + 2*PI) = C(t) +
- *   {0, 0, step}.
- */
 #include <iostream>
+#include <cmath>
 #include <mycadlib.h>
+#include <memory>
+#include <chrono>
+#include <random>
+#include <algorithm> //sort
 
 
 int main() {
@@ -45,34 +12,85 @@ int main() {
 
     using space_type = double;
 
-    ellipse3<space_type> ellipse(
-            0, 0,       // x, y
-            4, 3,       // a, b
-            8           // points_count
-    );
-    circle3<space_type> circle(
-            0, 0, 0,    // x, y, z
-            4,          // r
-            0, 360,     // angle_start, angle_end
-            4           // points_count
-    );
-    helix3<space_type> __helix(
-            0, 0, 0,    // x, y, z
-            4,          // r
-            1,          // step
-            10          // points_count
-    );
-    helix3<space_type> helix(
-            0, 0, 0,    // x, y, z
-            4, 4,       // a, b
-            1,          // step
-            0, 360*3,   // angle_start, angle_end
-            10          // points_count
-    );
+    std::mt19937_64 r(std::chrono::high_resolution_clock::now().time_since_epoch().count());
+    std::uniform_int_distribution<> dis_vector_size(10, 20);
+    std::uniform_int_distribution<> dis_curve_type(0, 2); // ellispe3, circle3, helix3
+    std::uniform_real_distribution<> dis_space(-10, 10);
+    std::uniform_real_distribution<> dis_positive(0, 10);
+    std::uniform_real_distribution<> dis_radians(0, M_PIl*2);
 
-    cout << "ellipse: " << ellipse << endl;
-    cout << "circle: " << circle << endl;
-    cout << "helix: " << helix << endl;
+    /// Populate a container (e.g. vector or list) of objects of these types created in random manner with random parameters
+    std::vector<std::shared_ptr<curve3<space_type>>> curves;
+    curves.reserve(dis_vector_size(r));
+    for(size_t i=0; i<curves.capacity(); i++) {
+        switch(dis_curve_type(r)) {
+            case 0:
+                curves.push_back(shared_ptr<curve3<space_type>>(
+                        new ellipse3<space_type>(dis_space(r), dis_space(r), dis_positive(r), dis_positive(r))));
+                break;
+            case 1:
+                curves.push_back(shared_ptr<curve3<space_type>>(
+                        new circle3<space_type>(dis_space(r), dis_space(r), dis_positive(r))));
+                break;
+            case 2:
+                curves.push_back(shared_ptr<curve3<space_type>>(
+                        new helix3<space_type>(dis_space(r), dis_space(r), dis_space(r), dis_positive(r), dis_positive(r))));
+        }
+    }
+
+    /// Populate a second container that would contain only circles from the first container
+    std::vector<circle3<space_type> *> circles;
+    size_t flags = 0;
+    for(auto& curve_p : curves) {
+        https://www.reddit.com/r/cpp_questions/comments/7x8jzj/help_with_understanding_error_expression_with/
+        auto curve = curve_p.get();
+        if (typeid(*curve) == typeid(ellipse3<space_type>)) {
+            flags |= 1u<<0u;
+        }
+        if (typeid(*curve) == typeid(circle3<space_type>)) {
+            flags |= 1u<<1u;
+            circles.push_back(dynamic_cast<circle3<space_type> *>(curve_p.get()));
+            if (&circles.back()->position != &curve_p.get()->position)
+                throw std::runtime_error("Make sure the second container shares (i.e. not clones) circles of the first one");
+        }
+        if (typeid(*curve) == typeid(helix3<space_type>)) {
+            flags |= 1u<<2u;
+        }
+    }
+    if (flags != 0b111)
+        throw std::runtime_error("Ensure that the container will contain objects of all supported types, flags: "+std::to_string(flags));
+
+    /// Print coordinates of points and derivatives of all curves in the container at t=PI/4.
+    for(auto& curve_p : curves) {
+        auto *curve = curve_p.get();
+        cout<< typeid(*curve).name()<<": C(t)="<<curve->calculate(M_PIl/4)<<", dC(t)/dt="<<curve->derivative(M_PIl/4)<<", where t=PI/4"<<endl;
+    }
+
+    /// Sort the second container in the ascending order of circles’ radii
+    std::sort(circles.begin(), circles.end(), [](circle3<space_type> *lhs, circle3<space_type> *rhs) {
+        return lhs->a < rhs->a;
+    });
+
+    /// That is, the first element has the smallest radius, the last - the greatest.
+    cout<<"first: "<<circles.front()->a<<", last: "<<circles.back()->a<<endl;
+
+    /// Compute the total sum of radii of all curves in the second container
+    space_type radii_sum = 0;
+    for(const auto& circle_p : circles)
+        radii_sum += circle_p->a;
+    cout<<"total sum of radii: "<<radii_sum<<endl;
+
+    /// any point on helix satisfies the condition C(t + 2*PI) = C(t) + {0, 0, step}.
+    helix3<space_type> h1(
+            1, 2, 3,    // x, y, z
+            5, 5,       // a, b
+            2,          // step
+            M_PIl/3     // angle_start
+    );
+    vector3<space_type> a = h1.calculate(10);
+    a.z += h1.step;
+    vector3<space_type> b = h1.calculate(10 + 2*M_PIl);
+    cout<<"a: "<<a<<", b: "<<b<<endl;
 
     return 0;
 }
--- mycadlib.h
+++ mycadlib.h
@@ -34,83 +34,40 @@ public:
 };
 
 
-//! for visuals
 template<class T>
-class mappable3 {
+class curve3 {
 public:
-    virtual void remap(size_t points_count) {
-        this->points.clear();
-        this->points.reserve(points_count);
-    }
+    curve3(T x, T y, T z)
+            : position(x, y, z) {}
 
-    std::string str() const {
-        std::ostringstream ss;
-        for (const vector3<T> &point : points)
-            ss << point << ",";
-        ss << "\b";
-        return ss.str();
-    }
+    virtual vector3<T> calculate(double angle) const = 0;
 
-    friend std::ostream &operator<<(std::ostream &os, const mappable3 &rhs) {
-        os << rhs.str();
-        return os;
-    }
-
-public:
-    std::vector<vector3<T>> points;
-};
-
-// TODO: curve3 should not inherit mappable3
-template<class T>
-class curve3 : public virtual mappable3<T> {
-public:
-    curve3(T x, T y, T z, T angle_start, T angle_end, size_t points_count)
-            : position(x, y, z), angle_start(angle_start), angle_end(angle_end) {}
-
-    virtual vector3<T> calculate(T angle) const = 0;
-
-    virtual vector3<T> derivative(T angle) const = 0;
+    virtual vector3<T> derivative(double angle) const = 0;
 
 public:
     vector3<T> position;
-    T angle_start;
-    T angle_end;
 };
 
 
 template<class T>
 class ellipse3 : public curve3<T> {
 public:
-    ellipse3(T x, T y, T a, T b, size_t points_count)
-            : ellipse3(x, y, 0, a, b, 0, 360, points_count) {}
+    ellipse3(T x, T y, T a, T b)
+            : ellipse3(x, y, static_cast<T>(0), a, b) {}
 
-    ellipse3(T x, T y, T z, T a, T b, T angle_start, T angle_end, size_t points_count)
-            : curve3<T>(x, y, z, angle_start, angle_end, points_count), a(a), b(b) {
-        remap(points_count);
-    }
+    ellipse3(T x, T y, T z, T a, T b)
+            : curve3<T>(x, y, z), a(a), b(b) {}
 
-    void remap(size_t points_count) override {
-        mappable3<T>::remap(points_count);
-
-        T angle_len = this->angle_end - this->angle_start;
-        T angle_cursor = this->angle_start;
-        for (size_t i = 0; i < points_count; i++) {
-            vector3 point = calculate(angle_cursor);
-            this->points.push_back(point);
-            angle_cursor = angle_len * (i + 1) / points_count;
-        }
-    }
-
-    vector3<T> calculate(T angle) const override {
+    vector3<T> calculate(double angle) const override {
         /// x=a*cos(t), y=b*sin(t), for t from 0 to 2*pi, where a=5, b=3
-        return vector3<T>(static_cast<T>(this->position.x + this->a * cos(angle * M_PI / 180)),
-                          static_cast<T>(this->position.y + this->b * sin(angle * M_PI / 180)),
+        return vector3<T>(static_cast<T>(this->position.x + a * cos(angle)),
+                          static_cast<T>(this->position.y + b * sin(angle)),
                           this->position.z);
     }
 
-    vector3<T> derivative(T angle) const override {
-        return vector3<T>(static_cast<T>(this->a * -sin(angle * M_PI / 180)),
-                          static_cast<T>(this->b * cos(angle * M_PI / 180)),
+    vector3<T> derivative(double angle) const override {
+        return vector3<T>(static_cast<T>(a * -sin(angle)),
+                          static_cast<T>(b * cos(angle)),
                           0);
     }
 
@@ -123,61 +80,46 @@ public:
 template<class T>
 class circle3 : public ellipse3<T> {
 public:
-    explicit circle3(T x, T y, T r, size_t points_count)
-            : circle3(x, y, 0, r, 0, 360, 0, 0, 0, points_count) {}
+    explicit circle3(T x, T y, T r)
+            : circle3(x, y, 0, r) {}
 
-    explicit circle3(T x, T y, T z, T r, T angle_start, T angle_end, size_t points_count)
-            : ellipse3<T>(x, y, z, r, r, angle_start, angle_end, points_count) {}
+    explicit circle3(T x, T y, T z, T r)
+            : ellipse3<T>(x, y, z, r, r) {}
 };
 
 
 template<class T>
-// class helix3 : public ellipse<T> <-- this is mistake, because first and last points in circle/ellipse are closed (замкнуты)
+// class helix3 : public ellipse<T, __Radians> <-- this is mistake, because first and last points in circle/ellipse are closed (замкнуты)
 class helix3 : public curve3<T> {
 public:
-    explicit helix3(T x, T y, T z, T r, T step, size_t points_count)
-            : helix3<T>(x, y, z, r, r, step, 0, 360, points_count) {}
+    explicit helix3(T x, T y, T z, T r, T step)
+            : helix3<T>(x, y, z, r, r, step, 0) {}
 
-    explicit helix3(T x, T y, T z, T a, T b, T step, T angle_start, T angle_end, size_t points_count)
-            : curve3<T>(x, y, z, angle_start, angle_end, points_count), a(a), b(b), step(step) {
-        remap(points_count);
-    }
+    explicit helix3(T x, T y, T z, T a, T b, T step, double angle_start)
+            : curve3<T>(x, y, z), a(a), b(b), step(step), angle_start(angle_start) {}
 
-    void remap(size_t points_count) override {
-        curve3<T>::remap(points_count);
-
-        T angle_len = this->angle_end - this->angle_start;
-        T angle_cursor = this->angle_start;
-        for (size_t i = 0; i < points_count; i++) {
-            vector3 point = calculate(angle_cursor);
-            this->points.push_back(point);
-            angle_cursor = angle_len * (i + 1) / (points_count - 1); // note changed here in difference with ellipse3::remap()
-        }
-    }
-
-    vector3<T> calculate(T angle) const override {
+    vector3<T> calculate(double angle) const override {
         /// x=a*cos(t), y=b*sin(t), for t from 0 to 2*pi, where a=5, b=3
         return vector3<T>(
-                static_cast<T>(this->position.x + this->a * cos(angle * M_PI / 180)),
-                static_cast<T>(this->position.y + this->b * sin(angle * M_PI / 180)),
+                static_cast<T>(this->position.x + a * cos(angle)),
+                static_cast<T>(this->position.y + b * sin(angle)),
                 // note changed here in difference with ellipse3::calculate()
-                this->position.z + (this->angle_start + angle)/360*step
-                // for the height:
-                // this->position.z + (this->angle_start + angle)/(this->angle_end - this->angle_start)*height
+                this->position.z + (angle_start + angle)/(M_PIl*2)*step
         );
     }
 
-    vector3<T> derivative(T angle) const override {
+    vector3<T> derivative(double angle) const override {
         return vector3<T>(
-                static_cast<T>(this->a * -sin(angle * M_PI / 180)),
-                static_cast<T>(this->b * cos(angle * M_PI / 180)),
+                static_cast<T>(a * -sin(angle)),
+                static_cast<T>(b * cos(angle)),
                 // https://www.wolframalpha.com/input/?i=d%2Fdx+z+%2B+(f+%2B+x)%2F360*s
-                step/360
+                step/(M_PIl*2)
         );
     }
 
-protected:
+public:
     T a;
     T b;
     T step;
+    double angle_start;
 };
